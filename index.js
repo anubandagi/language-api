d3745f02b17c4c32d00c45c8402c0e08a4b889db
anubandagi <anubandagi@gmail.com>
2024-07-16 21:23:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const languages=require("./output.json");
+const app=express();
+const port=8000;
+app.get("/languages",(req,res)=>{
+    return res.json(languages);
+})
+
+app.listen(port);
